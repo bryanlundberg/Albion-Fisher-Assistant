a2004088c5f6a65ae70fd73592597df07b39c6ea
rojonaitor <bryanlundberg@outlook.com>
2023-03-19 20:31:54 -0600

    Initial commit

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-// Move the mouse across the screen as a sine wave.
 const robot = require("robotjs");
 
-// Speed up the mouse.
-robot.setMouseDelay(2);
-
-const twoPI = Math.PI * 2.0;
-const screenSize = robot.getScreenSize();
-const height = (screenSize.height / 2) - 10;
-const width = screenSize.width;
-
-for (let x = 0; x < width; x++)
-{
-	y = height * Math.sin((twoPI * x) / width) + height;
-	robot.moveMouse(x, y);
+while (true) {
+	const mouse = robot.getMousePos();
+	const hex = robot.getPixelColor(mouse.x, mouse.y);
+	console.log("#" + hex + " at x:" + mouse.x + " y:" + mouse.y);
 }
