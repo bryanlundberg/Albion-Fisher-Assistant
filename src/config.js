028cf4b5108dae3261f043cdf1c2576b8872661d
rojonaitor <bryanlundberg@outlook.com>
2023-03-21 18:34:50 -0600

    minigame start

--- src/config.js
+++ src/config.js
@@ -1,10 +1,31 @@
 module.exports = {
   resolution: "1280x720",
   coordinates: {
-    x: 853,
-    y: 573,
-    w: 1065,
-    h: 596,
+    x: 857,
+    y: 577,
+    w: 1066,
+    h: 595,
     color: "c1d5fd"
-  }
+  },
+	gameReferenceLeft: {
+		x: 864,
+		y: 547,
+		w: 932,
+		h: 574,
+		color: "f52208"
+	},
+	gameReferenceRight: {
+		x: 1012,
+		y: 548,
+		w: 1061,
+		h: 574,
+		color: "f52208"
+	},
+	endGameReference: {
+		x: 857,
+		y: 290,
+		w: 883,
+		h: 293,
+		color: "fdd598"
+	},
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,73 @@
-const robot = require('robotjs');
-const pixelSearch = require("./modules/utils/pixelSearch")
+const robot = require("robotjs");
+const pixelSearch = require("./modules/utils/pixelSearch");
 const config = require("./config");
 
+let minigameStarted = null;
+let minigameFinished = true;
+let hold = false;
+
 function main() {
-	while (true) {
-		const fishIcon = pixelSearch(config.coordinates.x, config.coordinates.y, config.coordinates.w, config.coordinates.h, config.coordinates.color);
-		console.log(fishIcon)
-	}
+  while (minigameStarted === null) {
+    minigameStarted = pixelSearch(
+      config.coordinates.x,
+      config.coordinates.y,
+      config.coordinates.w,
+      config.coordinates.h,
+      config.coordinates.color
+    );
+    console.log("searching minigame");
+  }
+
+  console.log("minigame started");
+  robot.mouseToggle("down");
+  hold = true;
+  minigameFinished = false;
+  minigameStarted = null;
+
+  while ((minigameFinished = false)) {
+		console.log("minigame loop")
+    while (hold === true) {
+      console.log("searching end reference");
+      const endReference = pixelSearch(
+        config.endGameReference.x,
+        config.endGameReference.y,
+        config.endGameReference.w,
+        config.endGameReference.h,
+        config.endGameReference.color
+      );
+      if (endReference !== null) {
+        minigameFinished = true;
+        robot.mouseToggle("up");
+      }
+      console.log("searching right reference");
+      const rightRefence = pixelSearch(
+        config.gameReferenceRight.x,
+        config.gameReferenceRight.y,
+        config.gameReferenceRight.w,
+        config.gameReferenceRight.h,
+        config.gameReferenceRight.color
+      );
+      if (rightRefence !== null) {
+        hold = false;
+        robot.mouseToggle("up");
+      }
+    }
+
+    while (hold === false) {
+      console.log("searching left reference");
+      const leftReference = pixelSearch(
+        config.gameReferenceLeft.x,
+        config.gameReferenceLeft.y,
+        config.gameReferenceLeft.w,
+        config.gameReferenceLeft.h,
+        config.gameReferenceLeft.color
+      );
+      if (leftReference !== null) {
+        hold = true;
+        robot.mouseToggle("down");
+      }
+    }
+  }
 }
 
 main();
-
