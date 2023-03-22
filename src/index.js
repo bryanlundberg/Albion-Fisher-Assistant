7c595844065c26a9441f7d4b8e25ed84403997b7
Bryan Lundberg <bryanlundberg@outlook.com>
2023-03-22 12:10:54 -0600

    main function - working

--- src/index.js
+++ src/index.js
@@ -3,71 +3,89 @@ const pixelSearch = require("./modules/utils/pixelSearch");
 const config = require("./config");
 
 let minigameStarted = null;
-let minigameFinished = true;
+let minigameFinished = false;
 let hold = false;
 
 function main() {
-  while (minigameStarted === null) {
-    minigameStarted = pixelSearch(
-      config.coordinates.x,
-      config.coordinates.y,
-      config.coordinates.w,
-      config.coordinates.h,
-      config.coordinates.color
-    );
-    console.log("searching minigame");
-  }
-
-  console.log("minigame started");
-  robot.mouseToggle("down");
-  hold = true;
-  minigameFinished = false;
-  minigameStarted = null;
-
-  while ((minigameFinished = false)) {
-		console.log("minigame loop")
-    while (hold === true) {
-      console.log("searching end reference");
-      const endReference = pixelSearch(
-        config.endGameReference.x,
-        config.endGameReference.y,
-        config.endGameReference.w,
-        config.endGameReference.h,
-        config.endGameReference.color
+  while (true) {
+    while (minigameStarted === null) {
+      minigameStarted = pixelSearch(
+        config.coordinates.x,
+        config.coordinates.y,
+        config.coordinates.w,
+        config.coordinates.h,
+        config.coordinates.color
       );
-      if (endReference !== null) {
-        minigameFinished = true;
-        robot.mouseToggle("up");
-      }
-      console.log("searching right reference");
-      const rightRefence = pixelSearch(
-        config.gameReferenceRight.x,
-        config.gameReferenceRight.y,
-        config.gameReferenceRight.w,
-        config.gameReferenceRight.h,
-        config.gameReferenceRight.color
-      );
-      if (rightRefence !== null) {
-        hold = false;
-        robot.mouseToggle("up");
-      }
+      console.log("searching minigame");
     }
 
-    while (hold === false) {
-      console.log("searching left reference");
-      const leftReference = pixelSearch(
-        config.gameReferenceLeft.x,
-        config.gameReferenceLeft.y,
-        config.gameReferenceLeft.w,
-        config.gameReferenceLeft.h,
-        config.gameReferenceLeft.color
-      );
-      if (leftReference !== null) {
-        hold = true;
-        robot.mouseToggle("down");
+    console.log("minigame started");
+    robot.mouseToggle("down");
+    hold = true;
+    minigameFinished = false;
+    minigameStarted = null;
+
+    while (minigameFinished === false) {
+      console.log("minigame loop");
+      while (hold === true) {
+        console.log("searching end reference");
+        const endReference = pixelSearch(
+          config.endGameReference.x,
+          config.endGameReference.y,
+          config.endGameReference.w,
+          config.endGameReference.h,
+          config.endGameReference.color
+        );
+        if (endReference !== null) {
+          minigameFinished = true;
+					hold = false;
+          robot.mouseToggle("up");
+					break;
+        }
+        console.log("searching right reference");
+        const rightRefence = pixelSearch(
+          config.gameReferenceRight.x,
+          config.gameReferenceRight.y,
+          config.gameReferenceRight.w,
+          config.gameReferenceRight.h,
+          config.gameReferenceRight.color
+        );
+        if (rightRefence !== null) {
+          hold = false;
+          robot.mouseToggle("up");
+        }
+      }
+
+      while (hold === false) {
+        console.log("searching left reference");
+        const endReference = pixelSearch(
+          config.endGameReference.x,
+          config.endGameReference.y,
+          config.endGameReference.w,
+          config.endGameReference.h,
+          config.endGameReference.color
+        );
+        if (endReference !== null) {
+          minigameFinished = true;
+					hold = false;
+          robot.mouseToggle("up");
+					break;
+        }
+        const leftReference = pixelSearch(
+          config.gameReferenceLeft.x,
+          config.gameReferenceLeft.y,
+          config.gameReferenceLeft.w,
+          config.gameReferenceLeft.h,
+          config.gameReferenceLeft.color
+        );
+        if (leftReference !== null) {
+          hold = true;
+          robot.mouseToggle("down");
+        }
       }
     }
   }
 }
+
 
 main();
