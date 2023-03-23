9250d80b0ecd5162f5bd8281681e860f117c35c5
Bryan Lundberg <bryanlundberg@outlook.com>
2023-03-23 08:24:37 -0600

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -5,27 +5,27 @@ module.exports = {
     y: 577,
     w: 1066,
     h: 595,
-    color: "c1d5fd"
+    color: "c1d5fd",
   },
-	gameReferenceLeft: {
-		x: 855,
-		y: 521,
-		w: 963,
-		h: 549,
-		color: "ffb41f"
-	},
-	gameReferenceRight: {
-		x: 1000,
-		y: 521,
-		w: 1056,
-		h: 549,
-		color: "ffb41f"
-	},
-	endGameReference: {
-		x: 847,
-		y: 319,
-		w: 877,
-		h: 329,
-		color: "e6d544"
-	},
+  gameReferenceLeft: {
+    x: 855,
+    y: 521,
+    w: 963,
+    h: 549,
+    color: "ffb41f",
+  },
+  gameReferenceRight: {
+    x: 1000,
+    y: 521,
+    w: 1056,
+    h: 549,
+    color: "ffb41f",
+  },
+  endGameReference: {
+    x: 847,
+    y: 319,
+    w: 877,
+    h: 329,
+    color: "e6d544",
+  },
 };
